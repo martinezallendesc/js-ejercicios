4cf1bed77983ebb089ca5d8bbb61de8793e36273
Claudiazepam <martinezallendesclaudia@gmail.com>
2024-08-18 22:17:55 -0400

    ejercicio 1

---
+++ assets/js/script1.js
@@ -0,0 +1,14 @@
+console.log("Hola, estoy funcionando")
+
+
+
+function anadirborde(borde) {
+let logo = document.querySelector("#identitaria");
+logo.style.borderStyle = borde;
+}
+
+function anadirborde(borde) {
+    let logo = document.querySelector("#identitaria");
+    logo.style.borderStyle = borde;
+    logo.style.borderRadius = "50px"
+    }
