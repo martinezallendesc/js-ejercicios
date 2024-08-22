512867dae327775cb7c5a3fa728249470e39af10
Claudiazepam <martinezallendesclaudia@gmail.com>
2024-08-22 18:48:28 -0400

    ejercicio 1 al 22 ago

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,17 +1,25 @@
 console.log("Hola, estoy funcionando")
 
+const logo = document.querySelector("#identitaria");
 
-
+/* Intento 1
 function anadirborde(borde) {
-let logo = document.querySelector("#identitaria");
 logo.style.borderStyle = borde;
+logo.style.borderRadius = "300px";
+logo.style.padding = "20px";
 }
+*/
 
+logo.addEventListener("click", function() {
 
-        function anadirborde(borde) {
-        let logo = document.querySelector("#identitaria");
-        logo.style.borderStyle = borde;
-        logo.style.borderRadius = "300px";
-        logo.style.padding = "20px";
+        if (logo.style.border) /*Si el borde existe*/ {
+            logo.style.border = ""; /*Lo dejo vacío*/
+        } else {
+
+                
         }
+
+
+})
+
 
---
+++ assets/js/script2.js
@@ -0,0 +1,1 @@
+console.log("Estoy funcionando gracias a dios")
