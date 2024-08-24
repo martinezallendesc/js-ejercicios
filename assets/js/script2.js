734841a45a9a7ba70f41ef8cf8b9fd12758b8b1b
Claudiazepam <martinezallendesclaudia@gmail.com>
2024-08-24 16:54:21 -0400

    ejericio 3

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,1 +1,29 @@
 console.log("Estoy funcionando gracias a dios")
+
+function calcular() {
+//Seleccionar el input y su valor
+    const sticker1elemento = document.getElementById('sticker-1');
+    const sticker1valor = Number(sticker1elemento.value);
+    
+    const sticker2elemento = document.getElementById('sticker-2');
+    const sticker2valor = Number(sticker2elemento.value);
+    
+    const sticker3elemento = document.getElementById('sticker-3');
+    const sticker3valor = Number(sticker3elemento.value);
+    
+//Sumo el valor de los inputs
+    
+    const totalstickers = sticker1valor + sticker2valor + sticker3valor;
+    const parrafo = document.getElementById("resultado");
+
+
+//Ejecuto la función condicional
+
+    if (totalstickers <= 10)  {
+    parrafo.textContent = `Llevas ${totalstickers} stickers`; 
+    } else {
+        parrafo.textContent = `Llevas demasiados stickers`;   
+    }
+    
+    
+    }
---
+++ assets/js/script3.js
@@ -0,0 +1,41 @@
+console.log("Estoy funcionando")
+
+
+
+calcular() {
+//Seleccionar el input y su valor
+const campo1 = document.getElementById("n1");
+const d1 = Number(campo1.value);
+
+const campo2 = document.getElementById("n2");
+const d2 = Number(campo2.value);
+
+const campo3 = document.getElementById("n3");
+const d3 = Number(campo3.value);
+
+
+
+//Declaro los números de la contraseña
+
+const (contrasena = d1, d2, d3)
+const resultado = document.getElementById("validacion")
+
+
+//Ejecuto la funcion condicional
+if (contrasena === 9, 1, 1) {
+    resultado.textContent = `password 1 correcto`;
+} else {
+    resultado.textContent = `password incorrecto`
+}
+
+if (contrasena === 7, 1, 4) {
+    resultado.textContent = `password 1 correcto`;
+} else {
+    resultado.textContent = `password incorrecto`
+}
+
+}
+
+
+
+
