202e94a25f14923dc604dc0cf537084c33e50821
Claudiazepam <martinezallendesclaudia@gmail.com>
2024-08-24 17:36:53 -0400

    tercer intento

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -5,30 +5,30 @@ console.log("Estoy funcionando")
 function calcular() {
 //Seleccionar el input y su valor
 const campo1 = document.getElementById("n1");
-const d1 = Number(campo1.value);
+const d1 = (campo1.value);
 
 const campo2 = document.getElementById("n2");
-const d2 = Number(campo2.value);
+const d2 = (campo2.value);
 
 const campo3 = document.getElementById("n3");
-const d3 = Number(campo3.value);
+const d3 = (campo3.value);
 
 
 
 //Declaro los números de la contraseña
 
-const contrasena = ("d1", "d2", "d3");
+const contrasena = d1+d2+d3;
 const mensaje = document.getElementById("msje")
 
 
 //Ejecuto la funcion condicional
-if (contrasena = "9", "1", "1") {
+if (contrasena === 911) {
     mensaje.itextContent = `password 1 es correcto`;
 } else {
     mensaje.textContent = `password incorrecto`;
 }
 
-if (contrasena = "7", "1", "4") {
+if (contrasena === 714) {
     mensaje.textContent = `password 2 es correcto`;
 } else {
     mensaje.textContent = `password incorrecto`;
